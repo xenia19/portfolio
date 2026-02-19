7d0a087a9a91400c4b2d4a0edd355dff9bf78478
Ksenia <ksiusha.maestra@gmail.com>
2026-02-19 12:08:45 +0100

    fix: move assets to src and update imports for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- 1. Добавь это
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <--- 2. И это
-  ],
+  plugins: [react()],
+  base: '/portfolio/', // <--- ДОБАВИТЬ ЭТУ СТРОЧКУ ОБЯЗАТЕЛЬНО!
 })
