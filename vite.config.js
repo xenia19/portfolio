9f93b298accd1b67113b54587b31dc5c2c4269a6
Ksenia <ksiusha.maestra@gmail.com>
2026-02-19 12:33:19 +0100

    fix: added tailwindcss vite plugin for v4 build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <--- ЭТОТ ИМПОРТ НУЖЕН ДЛЯ TAILWIND V4
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/', // <--- ДОБАВИТЬ ЭТУ СТРОЧКУ ОБЯЗАТЕЛЬНО!
+  plugins: [
+    react(),
+    tailwindcss(), // <--- И ЭТОТ ПЛАГИН ТОЖЕ
+  ],
+  base: '/portfolio/',
 })
